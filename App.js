ca5f646066995f021451b9927912b75126d801d9
Albunist <61377132+Albunist@users.noreply.github.com>
2023-09-14 19:51:40 +0800

    Test integration

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import Toast from 'react-native-toast-message';
-import HomeScreen from "./src/screens/HomeScreen";
-import LoginScreen from "./src/screens/LoginScreen";
+import {HomeScreen} from "./src/screens/HomeScreen";
+import {LoginScreen} from "./src/screens/LoginScreen";
+import {ResetPasswordScreen} from "./src/screens/ResetPasswordScreen";
+import {ForgotPasswordScreen} from "./src/screens/ForgotPasswordScreen";
+import {SignUpScreen} from "./src/screens/SignUpScreen";
+import {CodeVerificationScreen} from "./src/screens/CodeVerificationScreen";
 import CreditCardsScreen from "./src/screens/CreditCard/CreditCardsScreen"
 import AddCreditCardScreen from './src/screens/CreditCard/AddCreditCardScreen';
 import CreditCardScreen from './src/screens/CreditCard/CreditCardScreen';
@@ -21,8 +25,13 @@ export default function App() {
   return (
       <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{title: 'Welcome'}}/>
-            <Stack.Screen name="LoginScreen" component={LoginScreen} options={{title: 'Login'}}/>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{title: 'Login '}}/>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{title: 'Login '}}/>
+            <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{title: 'Sign Up '}}/>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{title: 'Home '}}/>
+            <Stack.Screen name="CodeVerificationScreen" component={CodeVerificationScreen} options={{title: 'Verify Code'}}/>
+            <Stack.Screen name="ResetPasswordScreen" component={ResetPasswordScreen} options={{title: 'Reset Password'}}/>
+            <Stack.Screen name="ForgotPasswordScreen" component={ForgotPasswordScreen} options={{title: 'Forgot Password'}}/>
             <Stack.Screen name="CreditCardsScreen" component={CreditCardsScreen} options={{title: 'My Credit Cards'}}/>
             <Stack.Screen name="AddCreditCardScreen" component={AddCreditCardScreen} options={{title: 'Add Credit Card'}}/>
             <Stack.Screen name="CreditCardScreen" component={CreditCardScreen} options={{title: 'View Credit Card'}}/>

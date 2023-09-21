d99c7a0c433521020e044ca1ecd653f0393ab88d
Gabriel Lee <gabriel.lee@u.nus.edu>
2023-09-21 20:46:36 +0800

    Common function to catch 400, 404 and 422

--- src/redux/localRedux.js
+++ src/redux/localRedux.js
@@ -1,17 +1,12 @@
 import { localApi } from "../helpers/api";
+import { handleApiErrors } from "../helpers/errorCatching";
 
 export async function editLocalProfile(edittedProfile) {
   console.log("Enter editLocalProfile function");
   return await localApi.put(`/editLocalProfile`, edittedProfile)
   .then((response) => {
-    console.log(response);
-    if (response.data.httpStatusCode === 400 || response.data.httpStatusCode === 422) { // error
-      console.log('failure in localRedux :: editLocalProfile')
-      return {status: false, data: response.data};
-    } else { // success
-      console.log("success in localRedux :: editLocalProfile");
-      return {status: true, data: response.data};
-    }
+    console.log('in localRedux :: editLocalProfile')
+      handleApiErrors(response);
   })
   .catch((error) => {
     console.error("localRedux editLocalProfile: ", error);

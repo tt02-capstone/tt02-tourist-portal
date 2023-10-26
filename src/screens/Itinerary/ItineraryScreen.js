e93c0a6318fe9e81c66cadc501ffddbbcfedde7f
kellyco540 <kellyco540@gmail.com>
2023-10-26 20:07:36 +0800

    get itinerary

--- src/screens/Itinerary/ItineraryScreen.js
+++ src/screens/Itinerary/ItineraryScreen.js
@@ -8,39 +8,127 @@ import { useFocusEffect } from '@react-navigation/native';
 import { useIsFocused } from "@react-navigation/native";
 import { DatePickerModal } from 'react-native-paper-dates';
 import CreateItineraryScreen from './CreateItineraryScreen';
+import { IconButton } from 'react-native-paper';
 
 const ItineraryScreen = ({ navigation }) => {
-
+    const [user, setUser] = useState('');
     const isFocused = useIsFocused();
     const [range, setRange] = useState({ startDate: undefined, endDate: undefined });
     const [open, setOpen] = useState(false);
+    const [itinerary, setItinerary] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         async function onLoad() {
             const user = await getUser();
+            setUser(user);
         }
         onLoad();
+        fetchItinerary();
 
         if (isFocused) {
             onLoad();
+            fetchItinerary();
         }
 
     }, [isFocused]);
 
+    const fetchItinerary = async () => {
+        console.log("user.user_id", user.user_id);
+        try {
+            let response = await getItineraryByUser(user.user_id);
+            console.log("response");
+            if (response.status) {
+                console.log("response", response.data);
+                setItinerary(response.data);
+                setLoading(false);
+    
+            } else {
+                console.log("Itinerary not fetched!");
+            }
+        } catch (error) {
+            alert('An error occur! Failed to retrieve itinerary details!');
+            setLoading(false);
+        }
+    }
+
+    const handleDeleteItineraryPress = (itineraryId) => {
+
+        Alert.alert(
+            "Delete Confirmation",
+            "Are you sure you want to delete this itinerary?",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Delete Cancelled"),
+                    style: "cancel"
+                },
+                {
+                    text: "OK",
+                    onPress: () => handleDeleteItinerary(user.user_id, itineraryId)
+                }
+            ],
+            { cancelable: false }
+        );
+    };
+
+    async function handleDeleteItinerary(userId, itineraryId) {
+
+        let response = await deleteItinerary(userId, itineraryId);
+
+        if (response.status) {
+
+            Toast.show({
+                type: 'success',
+                text1: 'Itinerary deleted!'
+            })
+
+        } else {
+            console.log("Itinerary deletion failed!");
+            console.log(response.data);
+            Toast.show({
+                type: 'error',
+                text1: response.data.errorMessage
+            })
+        }
+    }
+
     return (
-        <Button text="+ Create Itinerary" style={styles.button} onPress={() => navigation.navigate('CreateItineraryScreen')} />
+        <View>
+            <Button text="+ Create Itinerary" style={styles.button} onPress={() => navigation.navigate('CreateItineraryScreen')} />
+            <View style={styles.iconContainer}>
+                {/* <IconButton
+                    icon="pencil"
+                    size={20}
+                    style={styles.icon}
+                    onPress={() => navigation.navigate('EditItineraryScreen', { itineraryId: item.itinerary_id, itineraryItineraryId: ItineraryId })}
+                /> */}
+                <IconButton
+                    icon="delete"
+                    size={20}
+                    style={styles.icon}
+                    onPress={() => handleDeleteItineraryPress(itinerary.itinerary_id)}
+                />
+            </View>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
     button: {
         width: '40%',
         marginLeft: '30%',
         marginRight: '30%',
         backgroundColor: '#5f80e3'
+    },
+    iconContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    icon: {
+        marginLeft: -5,
+        marginTop: -5,
+        marginRight: 3,
     },
 });
 

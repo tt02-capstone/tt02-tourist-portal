a6dbcdf07533ec80fcfe5ceba2f7d623f23be3c0
kellyco540 <kellyco540@gmail.com>
2023-10-27 10:13:27 +0800

    tab rendering for each day done

--- src/screens/Itinerary/ItineraryScreen.js
+++ src/screens/Itinerary/ItineraryScreen.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Background from '../../components/CardBackground'
 import Button from '../../components/Button'
-import { View, ScrollView, StyleSheet, TouchableOpacity, Modal, Alert } from 'react-native';
+import { View, ScrollView, StyleSheet, TouchableOpacity, Modal, Alert, Text } from 'react-native';
 import { getItineraryByUser, createItinerary, updateItinerary, deleteItinerary } from '../../redux/itineraryRedux';
 import { getUser, getUserType } from '../../helpers/LocalStorage';
 import { useFocusEffect } from '@react-navigation/native';
@@ -10,6 +10,7 @@ import { DatePickerModal } from 'react-native-paper-dates';
 import CreateItineraryScreen from './CreateItineraryScreen';
 import { IconButton } from 'react-native-paper';
 import Toast from "react-native-toast-message";
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
 
 const ItineraryScreen = ({ navigation }) => {
     const [user, setUser] = useState('');
@@ -18,6 +19,8 @@ const ItineraryScreen = ({ navigation }) => {
     const [open, setOpen] = useState(false);
     const [itinerary, setItinerary] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [index, setIndex] = useState(0);
+    const [routes, setRoutes] = useState([]);
 
     useEffect(() => {
         async function onLoad() {
@@ -32,8 +35,16 @@ const ItineraryScreen = ({ navigation }) => {
                 console.log("response.data", response.data)
                 setItinerary(response.data);
                 setLoading(false);
+
+                const numberOfDays = calculateNumberOfDays(response.data.start_date, response.data.end_date);
+                const generatedRoutes = [];
+                for (let i = 0; i < numberOfDays; i++) {
+                    generatedRoutes.push({ key: `day${i + 1}`, title: `Day ${i + 1}` });
+                }
+                setRoutes(generatedRoutes);
+                console.log("routes", routes);
             } catch (error) {
-                alert('An error occurred! Failed to retrieve supportTicket list!');
+                alert('An error occurred! Failed to retrieve itinerary!');
                 setLoading(false);
             }
         }
@@ -44,6 +55,15 @@ const ItineraryScreen = ({ navigation }) => {
         }
 
     }, [isFocused]);
+
+    // Function to calculate the number of days between two dates
+    const calculateNumberOfDays = (startDate, endDate) => {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+        const timeDifference = end - start;
+        const daysDifference = timeDifference / (1000 * 3600 * 24);
+        return Math.floor(daysDifference) + 1;
+    };
 
     const handleDeleteItineraryPress = (itineraryId) => {
 
@@ -91,8 +111,22 @@ const ItineraryScreen = ({ navigation }) => {
         }
     }
 
+    const renderScene = ({ route }) => {
+        switch (route.key) {
+            case 'day1':
+                return <View><Text>Day 1 Itinerary</Text></View>; // Replace with your content
+            default:
+                const dayNum = parseInt(route.key.replace('day', ''));
+                return <View><Text>Day {dayNum} Itinerary</Text></View>;
+        }
+    }
+
+    const calculateTabWidth = () => {
+        return routes.length <= 4 ? 390 / routes.length : 87;
+    };
+
     return (
-        <View>
+        <ScrollView>
             <Button text="+ Create Itinerary" style={styles.button} onPress={() => navigation.navigate('CreateItineraryScreen')} />
             <View style={styles.iconContainer}>
                 <IconButton
@@ -108,8 +142,23 @@ const ItineraryScreen = ({ navigation }) => {
                     onPress={() => handleDeleteItineraryPress(itinerary.itinerary_id)}
                 />
             </View>
-        </View>
-    )
+
+            <TabView
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                initialLayout={{ width: '100%' }}
+                scrollEnabled={true}
+                renderTabBar={props => (
+                    <TabBar 
+                        {...props} 
+                        scrollEnabled={true} 
+                        tabStyle={{ width: calculateTabWidth() }} 
+                    />
+                )}
+            />
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
